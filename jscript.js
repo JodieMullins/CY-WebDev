037d309680a67af757c368c3781bd1ffb6016cac
Jodie Mullins <jmullins2460@gmail.com>
2024-05-19 16:16:54 -0400

    continue pseudocode shoesize converter logic

--- jscript.js
+++ jscript.js
@@ -9,10 +9,15 @@
 function shoeConverter(sizeNum) {
     // convert user input to integer
   sizeNum = parseFloat(sizeNum);
-  
+
   // if W option is selected, 
   document.getElementById("converted-size-display").innerHTML= (sizeNum - 2)
   // if M / Y option is selected, 
   document.getElementById("converted-size-display").innerHTML= (sizeNum + 2)
+  // M max 12 min 2
+  // W max 14 min 4
+  
+  // If M selected AND shoe size goes above 12
+  // " You may struggle finding styles in that size"
 }
 
