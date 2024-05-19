74330093870c42b9fca6b6ff8a1ae9023b75437a
Jodie Mullins <jmullins2460@gmail.com>
2024-05-19 15:55:15 -0400

    began pseudo-logic for shoe size converter

--- jscript.js
+++ jscript.js
@@ -6,3 +6,13 @@
  * 
  ***/
 
+function shoeConverter(sizeNum) {
+    // convert user input to integer
+  sizeNum = parseFloat(sizeNum);
+  
+  // if W option is selected, 
+  document.getElementById("converted-size-display").innerHTML= (sizeNum - 2)
+  // if M / Y option is selected, 
+  document.getElementById("converted-size-display").innerHTML= (sizeNum + 2)
+}
+
