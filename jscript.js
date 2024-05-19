cc9237707ac470209d7fcf9c5a047dc611663541
Jodie Mullins <jmullins2460@gmail.com>
2024-05-19 16:38:37 -0400

    simplified radio buttons, removed youth sizes, shoeConverter pseudo

--- jscript.js
+++ jscript.js
@@ -6,18 +6,20 @@
  * 
  ***/
 
-function shoeConverter(sizeNum) {
-    // convert user input to integer
-  sizeNum = parseFloat(sizeNum);
+function shoeConverter(sizeIn) {
+    // convert user input to float (because half sizes)
+    sizeIn = parseFloat(sizeIn);
 
   // if W option is selected, 
-  document.getElementById("converted-size-display").innerHTML= (sizeNum - 2)
+  document.getElementById("converted-size-display").innerHTML= (sizeIn - 2)
   // if M / Y option is selected, 
-  document.getElementById("converted-size-display").innerHTML= (sizeNum + 2)
+  document.getElementById("converted-size-display").innerHTML= (sizeIn + 2)
   // M max 12 min 2
   // W max 14 min 4
   
-  // If M selected AND shoe size goes above 12
-  // " You may struggle finding styles in that size"
+  // If M selected AND shoe size  > 12
+  // "It may be difficult finding styles in that size."
+
+    // site does NOT contain youth sizes
 }
 
