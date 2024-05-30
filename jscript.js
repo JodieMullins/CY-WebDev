417f5a2a2848c89329139076a567492908c8e519
Jodie Mullins <jmullins2460@gmail.com>
2024-05-30 18:14:41 -0400

    Halloween Sale Countdown added

--- jscript.js
+++ jscript.js
@@ -13,8 +13,46 @@ function temperatureConverter(valNum) {
 
     // grab this item and display the output of 
     // an equation to translate Celcius into Fahrenheit 
-    document.getElementById("outputFahrenheit").innerHTML=(valNum * 1.8) +32 ;
+    document.getElementById("outputFahrenheit").innerText=(valNum * 1.8) +32 ;
 }
 
 
 // is it best practice to have multiple .js files??
+
+
+
+/***
+ * 
+ *          PROMO 
+ * 
+ ***/
+
+// grab today's date
+let today = new Date();
+// detail today's year
+let saleYear = today.getFullYear();
+
+// if the date is after October 31st (so any time November 1 and later)
+if (today.getMonth() == 9 && today.getDate() > 31) {
+
+    // add another year until the sale date
+    saleYear = saleYear + 1;
+}
+
+// create variable saleDate and set it equal to October 31st of closest upcoming sale
+let saleDate = new Date(saleYear, 9, 31);
+
+// calculating ms total of time in a day
+let dayMilliseconds = 1000 * 60 * 60 * 24;
+
+// Find remaining days until next sale
+let remainingDays = Math.ceil(
+    (saleDate.getTime() - today.getTime()) /
+    (dayMilliseconds)
+);
+
+let saleMessage = "There are " + remainingDays + " days remaining until the next HUGE Halloween sale!"
+
+
+
+document.getElementById("countdown-here").innerText = saleMessage
