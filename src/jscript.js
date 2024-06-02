689bb6e1e9ead40d5f51c74161017504a55ff01b
Jodie Mullins <jmullins2460@gmail.com>
2024-06-01 21:12:53 -0400

    autocomplete search bar not working??

--- src/jscript.js
+++ src/jscript.js
@@ -6,8 +6,96 @@
  * 
  ***/
 
-let autocompleteChoices = ["sale", "blue", "red", "red sole", "yellow", "green", "black", "purple", "orange", "white", "gold", "silver", "metallic", "new", "sneaker", "pumps", "heels"]
 
+/***
+// create array to hold words that will autocomplete
+let autocompleteChoices = ["sale", "speaker", "blue", "red", "red sole", "yellow", "green", "black", "bluetooth", "purple", "orange", "white", "gold", "silver", "metallic", "new", "brown", "sneaker", "pumps", "heels"]
+
+// create variables to reference input text where they need to go
+const searchbars = document.getElementsByClassName("search-bars");
+let resultsHTML = document.getElementById("autocomplete-display");
+
+searchbars.oninput = function () {
+
+    // create an empty array
+    let autoresults = [];
+
+    // .this references search bar object, from which 
+    // a value exists, captured within the variable userInput    
+    const userInput = this.value;
+
+    // use javascript to display an empty string value for the unordered list displaying from the search bar
+    resultsHTML.innerText = "";
+
+    // in the instance a user starts typing into the search bar,
+    if (userInput.length > 0) {
+        // the empty array is to return information based on user's input
+        autoresults = getResults(userInput);
+
+        for (i = 0; i <autoresults.length; i++) {
+            resultsHTML.innerHTML += "<li>" + autoresults[i] + "</li>";
+        }
+    }
+
+} // END searchbars.oninput function
+
+
+// DEFINE getResults
+function getResults(input) {
+
+    const results = [];
+
+    for (i = 0; i < autocompleteChoices.length; i++) {
+        if (input === autocompleteChoices[i].slice(0, input.length)) {
+            results.push(autocompleteChoices[i]);
+
+        }
+    }
+
+
+    return results;
+} // END getResults
+
+resultsHTML.onclick = function (event) {
+    const setValue = event.target.innerText;
+    searchbars.value = setValue;
+    this.innerText = "";
+};
+ ***/
+let data = ["sale", "speaker", "blue", "red", "red sole", "yellow", "green", "black", "bluetooth", "purple", "orange", "white", "gold", "silver", "metallic", "new", "brown", "sneaker", "pumps", "heels"];
+
+const autocomplete = document.getElementsByClassName("search-bars");
+const resultsHTML = document.getElementById("results");
+
+
+autocomplete.oninput = function () {
+    let results = [];
+    const userInput = this.value;
+    resultsHTML.innerHTML = "";
+    if (userInput.length > 0) {
+      results = getResults(userInput);
+      resultsHTML.style.display = "block";
+      for (i = 0; i < results.length; i++) {
+        resultsHTML.innerHTML += "<ul>" + results[i] + "</ul>";
+      }
+    }
+  };
+
+  function getResults(input) {
+    const results = [];
+    for (i = 0; i < data.length; i++) {
+      if (input === data[i].slice(0, input.length)) {
+        results.push(data[i]);
+      }
+    }
+    return results;
+  }
+
+  resultsHTML.onclick = function(event) {
+    const setValue = event.target.innerText;
+    autocomplete.value = setValue;
+    this.innerHTML = "";
+  };
 
 
 /***
