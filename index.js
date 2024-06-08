bfc6340d01a367b825c972913c8a53fa3cac0a99
Jodie Mullins <jmullins2460@gmail.com>
2024-06-08 16:21:15 -0400

    add PUT method

--- index.js
+++ index.js
@@ -120,6 +120,36 @@ router.post('/', function (req, res, next) {
 })
 
 
+router.put('/:id', function (req, res, next) {
+  shoeInfo.getById(req.params.id, function (data) {
+    if (data) {
+      // Attempt to update the data
+      shoeInfo.update(req.body, req.params.id, function (data) {
+        res.status(200).json({
+          "status": 200,
+          "statusText": "OK",
+          "message": "Shoes '" + req.params.id + "' updated.",
+          "data": data
+        });
+      });
+    }
+    else {
+      res.status(404).send({
+        "status": 404,
+        "statusText": "Not Found",
+        "message": "The pair of shoes '" + req.params.id + "' could not be found.",
+        "error": {
+          "code": "NOT_FOUND",
+          "message": "The pair of shoes '" + req.params.id + "' could not be found."
+        }
+      });
+    }
+  }, function(err) {
+    next(err);
+  });
+})
+
+
 // Configure router so all routes are prefixed with /api/v1
 app.use('/api/', router);
 // ALL REST APIs in this server are called:
