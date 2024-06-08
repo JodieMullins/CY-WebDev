3efc1470ffabe4fb0e77ac5c38baee0a7a03b615
Jodie Mullins <jmullins2460@gmail.com>
2024-06-08 16:22:47 -0400

    add delete method

--- index.js
+++ index.js
@@ -150,6 +150,37 @@ router.put('/:id', function (req, res, next) {
 })
 
 
+router.delete('/:id', function (req, res, next) {
+  shoeInfo.getById(req.params.id, function (data) {
+    if (data) {
+      // Attempt to delete the data
+      shoeInfo.delete(req.params.id, function (data) {
+        res.status(200).json({
+          "status": 200,
+          "statusText": "OK",
+          "message": "The shoes '" + req.params.id + "' have been deleted.",
+          "data": "Shoes '" + req.params.id + "' deleted."
+        });
+      });
+    }
+    else {
+      res.status(404).send({
+        "status": 404,
+        "statusText": "Not Found",
+        "message": "The shoes '" + req.params.id + "' could not be found.",
+        "error": {
+          "code": "NOT_FOUND",
+          "message": "The shoes '" + req.params.id + "' could not be found."
+        }
+      });
+    }
+  }, function(err) {
+    next(err);
+  });
+})
+
+
+
 // Configure router so all routes are prefixed with /api/v1
 app.use('/api/', router);
 // ALL REST APIs in this server are called:
