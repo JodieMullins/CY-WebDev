0d8f9d6231a0dfa9cc28bd074cbbea35f2915864
Jodie Mullins <jmullins2460@gmail.com>
2024-06-09 16:38:31 -0400

    comments and corrections

--- repos/logRepo.js
+++ repos/logRepo.js
@@ -4,10 +4,15 @@ const FILE_NAME = './logs/log.txt';
 
 let logRepo = {
   write: function (data, resolve, reject) {
+    // write file (create string file)
     let toWrite = "*".repeat(80) + "\r\n";
+    // write data 
     toWrite += "Date/Time: " + new Date().toLocaleDateString() + "\r\n";
+    // write error that occurred
     toWrite += "Exception Info: " + JSON.stringify(data) + "\r\n";
+    // ***********************************
     toWrite += "*".repeat(80) + "\r\n";
+
     fs.writeFile(FILE_NAME, toWrite, function (err) {
       if (err) {
         reject(err);
@@ -19,4 +24,5 @@ let logRepo = {
   }
 };
 
+// export as module responsible for logging 
 module.exports = logRepo;
