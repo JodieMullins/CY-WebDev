912829ed6e4334c5a3e610b415b615b10f6d25c1
Jodie Mullins <jmullins2460@gmail.com>
2024-06-17 22:01:58 -0400

    windows debug instructions

--- theKQapp/app.js
+++ theKQapp/app.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const debug = require('debug')('app');
 
+/**
+ * 
+ * to activate debug in Windows:
+ * 
+
+ * npm install --save-dev cross-env
+ * 
+ * {
+  "scripts": {
+    "build": "cross-env NODE_ENV=production webpack --config build/webpack.config.js"
+  }
+}
+
+
+ * in terminal,type:
+ * > set DEBUG=* & node app.js
+ * 
+ */
+
 const app = express();
 
 app.get('/', (req, res) => {
