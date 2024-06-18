fb6c37b703689b375f1dbb6157f38d9e76fc4126
Jodie Mullins <jmullins2460@gmail.com>
2024-06-17 21:36:29 -0400

    express app established

--- theKQapp/app.js
+++ theKQapp/app.js
@@ -1,3 +1,11 @@
 const express = require('express');
 
 const app = express();
+
+app.get('/', (req, res) => {
+    req.send('Hello from my app');
+})
+
+app.listen(3000, () =>{
+    console.log('listening on port 3000')
+})
